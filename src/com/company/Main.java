f5b4fb80d65056f8346d58b3dfd8112b5b8f1699
SohipKim <SoHeeKim@SoheeKim-ui-MBP.hitronhub.home>
2021-08-09 13:01:47 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        ComputerWage obj = new ComputerWage();
+
+        obj.AcceptData();
+        obj.ComputeWage();
+        obj.Display();
+
+
+
+    }
+}
